bf745d28720d71fe3ffa43c0824f4aac2a671f3f
unknown <leonardo.amaris@udea.edu.co>
2023-09-07 22:25:52 -0500

    feat: creacion de funciones en Function.java

--- src/main/java/com/mycompany/calculator/back/Functions.java
+++ src/main/java/com/mycompany/calculator/back/Functions.java
@@ -8,6 +8,33 @@ package com.mycompany.calculator.back;
  *
  * @author leona
  */
-public class Functions {
-    
+public class Functions implements Operations{
+    // Aritmetics operations
+    public double addition(double a, double b){
+        return a + b;
+    };
+    public double subtraction(double a, double b){
+        return a - b;
+    };
+    public double multiplication(double a, double b){
+        return a * b;
+    };
+    public double division(double a, double b){
+        return a / b;
+    };
+    public double rest(double a, double b){
+        return a - (int)division(a, b) * b;
+    };
+    public boolean minusEqualTo(double a, double b){
+        return a <= b;
+    };
+    public boolean greaterIgualQue(double a, double b){
+        return a >= b;
+    };
+    public double backwardUnitIncrement(double a){
+        return a + 1;
+    };
+    public double forwardUnitIncrement(double a, double b){
+        return a - 1;
+    };
 }
