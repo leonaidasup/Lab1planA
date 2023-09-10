fb22880f101d529c964e6a629798b2fa1613fa2c
unknown <leonardo.amaris@udea.edu.co>
2023-09-10 11:21:35 -0500

    feat: finalizacion de BooleanOperator.java e BooleanInterpreter.Java

--- src/main/java/com/mycompany/calculator/back/OperationCalculator/BooleanOperator.java
+++ src/main/java/com/mycompany/calculator/back/OperationCalculator/BooleanOperator.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.calculator.back.OperationCalculator;
+import com.mycompany.calculator.back.ElementalEspecialFunctions.ElementalFuncionts.BooleanFunctions;
 
 /**
  *
@@ -11,6 +12,26 @@ package com.mycompany.calculator.back.OperationCalculator;
 public class BooleanOperator {
     public BooleanOperator() {}
     public boolean result(boolean var1, boolean var2, String operator) {
-        return true;
+        switch (operator) {
+            case "!" -> {
+                return BooleanFunctions.negation(var1);
+            }
+            case "|" -> {
+                return BooleanFunctions.booleanOr(var1, var2);
+            }
+            case "^" -> {
+                return BooleanFunctions.exclusiveAddition(var1, var2);
+            }
+            case "&" -> {
+                return BooleanFunctions.booleanAnd(var1, var2);
+            }
+            case "||" -> {
+                return BooleanFunctions.booleanAddition(var1, var2);
+            }
+            case "&&" -> {
+                return BooleanFunctions.booleanProduct(var1, var2);
+            }    
+        }
+        return false;
     }
 }
